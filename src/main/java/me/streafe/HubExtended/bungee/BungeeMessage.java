277346e2f63c94cbcab214f6a9b7facf6dd67434
BuildTools <unconfigured@null.spigotmc.org>
2019-12-13 18:22:41 +0100

    commit 6:13 PM

--- src/main/java/me/streafe/HubExtended/bungee/BungeeMessage.java
+++ src/main/java/me/streafe/HubExtended/bungee/BungeeMessage.java
@@ -1,18 +1,35 @@
 package me.streafe.HubExtended.bungee;
 
+import me.streafe.HubExtended.HubExtended;
+import me.streafe.HubExtended.utils.Utils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class BungeeMessage implements CommandExecutor {
+
+    private BungeeMessageListener bListener = new BungeeMessageListener();
+    private Utils utils = new Utils();
+
     @Override
     public boolean onCommand(CommandSender sender, Command c, String s, String args[]) {
         if(sender instanceof Player){
             Player player = (Player) sender;
 
-            if(c.getName().equalsIgnoreCase("msg")){
+            if(c.getName().equalsIgnoreCase("connect")){
+                if(args.length == 1){
 
+                    player.sendMessage(utils.translate("&aConnection to " + args[0] + "..."));
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            bListener.connectToServer(player,args[0]);
+                            this.cancel();
+                        }
+                    }.runTaskLaterAsynchronously(HubExtended.getInstance(),40L);
+                }
             }
         }
         return true;
--- src/main/java/me/streafe/HubExtended/bungee/BungeeMessageListener.java
+++ src/main/java/me/streafe/HubExtended/bungee/BungeeMessageListener.java
@@ -1,7 +1,9 @@
 package me.streafe.HubExtended.bungee;
 
 import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import me.streafe.HubExtended.HubExtended;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
@@ -14,6 +16,20 @@ public class BungeeMessageListener implements PluginMessageListener {
             return;
         }
 
+        ByteArrayDataInput in = ByteStreams.newDataInput(message);
+        String subchannel = in.readUTF();
+
+        if(subchannel.equals("")){
+
+        }
+
+    }
+
+    public void connectToServer(Player player, String server){
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF("Connect");
+        out.writeUTF(server);
+        player.sendPluginMessage(HubExtended.getInstance(),"BungeeCord",out.toByteArray());
 
     }
 }
